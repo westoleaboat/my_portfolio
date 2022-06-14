8eaf49e05b21e92374f337bbb0f11f40268d2abc
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2022-06-14 15:25:43 +0300

    format change

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index),       # this will invoke whatever is defined in views.py
     #path('blog/index.html', views.index),
-    path('blog/graphs.html', views.graphs),
-    path('blog/guis.html', views.guis),
+    #path('blog/graphs.html', views.graphs),
+    #path('blog/guis.html', views.guis),
 ]
