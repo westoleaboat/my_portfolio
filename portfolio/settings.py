ad9635de2408c56f548ff60bbbaa671e76f7fdd5
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2022-06-18 18:44:37 +0300

    fixed layout

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = ''
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
